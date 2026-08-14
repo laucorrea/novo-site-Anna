a0c9c01a073c10eaa823b57b1394f8c9fdf56207
laucorrea <anna.correa.silva@escola.pr.gov.br>
2026-08-13 21:43:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-const botoes = document.querySelectorAll("button");
+// =========================
+// BOTÕES DE REAÇÃO
+// =========================
+
+const botoes = document.querySelectorAll(".post button");
 
 botoes.forEach(function (botao) {
+
     let curtiu = false;
-    botao.addEventListener("click", botaoClicado);
-    function botaoClicado() {
-        console.log("fui clicado");
-        let texto = botao.querySelector("span");
+
+    botao.addEventListener("click", function () {
+
+        const texto = botao.querySelector("span");
+
         if (curtiu === false) {
+
             texto.textContent++;
             curtiu = true;
+
         } else {
+
             texto.textContent--;
             curtiu = false;
+
         }
-    }
+
+    });
+
 });
+
+
+// =========================
+// TEMA ESCURO
+// =========================
 
 const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
 btnTemaEscuro.addEventListener("click", mudaTema);
 
+
 function mudaTema() {
+
     const corpoPagina = document.body;
+
     if (corpoPagina.classList.contains("tema-escuro")) {
+
         corpoPagina.classList.remove("tema-escuro");
+
+        btnTemaEscuro.textContent = "🌙";
+
     } else {
+
         corpoPagina.classList.add("tema-escuro");
+
+        btnTemaEscuro.textContent = "☀️";
+
     }
+
 }
